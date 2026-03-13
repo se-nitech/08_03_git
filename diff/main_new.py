8fef41911ccd337b84f571e601b3a69853485f1e
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2026-03-13 12:22:14 +0900

    merge diff and patch examples

---
+++ diff/main_new.py
@@ -0,0 +1,24 @@
+
+
+def myfunc(x, y):
+    z = 2 * x - y
+    return z
+
+
+def myfunc_3(a):
+    for i in range(5):
+        myfunc(a[i], a[i + 1])
+
+def myfunc_4(a):
+    z = a[1] + a[2]
+
+def main():
+    a = [1, 2, 3, 4, 5]
+    for _ in range(100):
+        myfunc_4(a)
+    for _ in range(300):
+        myfunc_3(a)
+
+
+if __name__ == '__main__':
+    main()
